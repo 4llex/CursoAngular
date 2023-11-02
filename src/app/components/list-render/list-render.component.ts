f04427102837cc32e811ed49163fb8a6304e5e14
Alex Rosa <alexr@gec.inatel.br>
2023-11-02 17:06:10 -0300

    Renderização de lista - *ngFor

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent {
+
+
+  animals = [
+    {name: "turca", type: "Dog"},
+    {name: "Tom", type: "Cat"},
+    {name: "Frida", type: "Dog"},
+    {name: "Bob", type: "Horse"}
+  ]
+
+}
