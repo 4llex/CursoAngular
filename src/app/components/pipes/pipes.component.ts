26fff40a1b5d119e1457d5782813628fbd6d4d30
Alex Rosa <alexr@gec.inatel.br>
2023-11-03 11:08:29 -0300

    trabalhando com pipe operators

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css']
+})
+export class PipesComponent {
+
+  texto: string = 'Testando pipe operator'
+  dateNow = new Date()
+
+}
